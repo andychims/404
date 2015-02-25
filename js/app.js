53512e299f9c7b4e5176d878f0daa6638df81891
Joseph McLean <sedad@Josephs-MacBook-Pro.local>
2015-02-25 17:10:33 -0500

    Add kick sound and play

--- js/app.js
+++ js/app.js
@@ -1,24 +1,42 @@
+$( document ).ready(function() {
 
-setInterval(triggerStep, 500);
+  setInterval(triggerStep, 500);
 
-var step = 0;
+  var kick_element = document.getElementById("kick-sound");
 
-function triggerStep(){
-  console.log(step);
+  var step = 0;
 
-  var activeStepID = "#step"+step.toString();
+  var kick_pattern = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
 
-  $(".counter").removeClass("counter-on");
-  $(activeStepID).addClass("counter-on");
+  function triggerStep(){
 
-  step++;
-  if (step > 15) {
-    step = 0;
-  }
-};
+    var activeStepID = "#step"+step.toString();
 
-$(".pad").click(function(){
-  console.log("yoyoyo");
+    $(".counter").removeClass("counter-on");
+    $(activeStepID).addClass("counter-on");
+
+    if(kick_pattern[step] == 1) {
+      kick_element.currentTime = 0;
+      kick_element.play();
+    } else { 
+      // console.log("no kick");
+    }
+
+    step++;
+    if (step > 15) {
+      step = 0;
+    }
+  };
+
+  $(".pad").click(function(){
+    $(this).find(".counter").toggleClass("pad-on");
+    var pad_number = $(this).attr('rel');
+    if (kick_pattern[pad_number] == 1) {
+      kick_pattern[pad_number] = 0;
+    } else {
+      kick_pattern[pad_number] = 1;
+    }
+  });
 });
 
 // $( ".pad" ).on( "click", function() {
