f833a27f37ab8c84928507d5ededc3bd4a22f550
andysplice <andy@splice.com>
2015-02-25 16:27:22 -0500

    idk what i updated

--- js/app.js
+++ js/app.js
@@ -5,8 +5,22 @@ var step = 0;
 
 function triggerStep(){
   console.log(step);
+
+  var activeStepID = "#step"+step.toString();
+
+  $(".counter").removeClass("counter-on");
+  $(activeStepID).addClass("counter-on");
+
   step++;
   if (step > 15) {
     step = 0;
   }
-}
+};
+
+$(".pad").click(function(){
+  console.log("yoyoyo");
+});
+
+// $( ".pad" ).on( "click", function() {
+//   console.log("hello");
+// });
