5d4bb4a84442f3746153a1c96a2aaa85eff7a1be
Joseph McLean <sedad@Josephs-MacBook-Pro.local>
2015-02-25 18:25:22 -0500

    Add snare sound, and refactor

--- js/app.js
+++ js/app.js
@@ -1,12 +1,40 @@
 $( document ).ready(function() {
 
+
+  // var Car = function(name) {
+  //   var self = this;
+  //   self.speed = 0;
+  //   self.name = name;
+
+  //   self.beep = function(){
+  //     console.log(self.name + " says beep");
+  //   }
+  // }
+
+  var Sound = function(name, element_id) {
+    var self = this;
+    self.pattern = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+    self.name = name;
+    self.sound_element = document.getElementById(element_id);
+  }
+
+  var kick = new Sound("kick", "kick-sound");
+  var snare = new Sound("snare", "snare-sound");
+
+  //
+  // init
+  //
+
   setInterval(triggerStep, 500);
 
-  var kick_element = document.getElementById("kick-sound");
+  var activeSound = kick
+  var activePattern = kick.pattern;
 
   var step = 0;
 
-  var kick_pattern = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+  //
+  //behavior every beat
+  //
 
   function triggerStep(){
 
@@ -15,12 +43,15 @@ $( document ).ready(function() {
     $(".counter").removeClass("counter-on");
     $(activeStepID).addClass("counter-on");
 
-    if(kick_pattern[step] == 1) {
-      kick_element.currentTime = 0;
-      kick_element.play();
-    } else { 
-      // console.log("no kick");
-    }
+    if(kick.pattern[step] == 1) {
+      kick.sound_element.currentTime = 0;
+      kick.sound_element.play();
+    } 
+
+    if(snare.pattern[step] == 1) {
+      snare.sound_element.currentTime = 0;
+      snare.sound_element.play();
+    }     
 
     step++;
     if (step > 15) {
@@ -28,17 +59,46 @@ $( document ).ready(function() {
     }
   };
 
+  //
+  // ui linking
+  //
+
+  //pad click
   $(".pad").click(function(){
     $(this).find(".counter").toggleClass("pad-on");
     var pad_number = $(this).attr('rel');
-    if (kick_pattern[pad_number] == 1) {
-      kick_pattern[pad_number] = 0;
-    } else {
-      kick_pattern[pad_number] = 1;
-    }
-  });
-});
 
-// $( ".pad" ).on( "click", function() {
-//   console.log("hello");
-// });
+    if (activeSound.pattern[pad_number] == 1) {
+      activeSound.pattern[pad_number] = 0;
+    } else {
+      activeSound.pattern[pad_number] = 1;
+    }
+
+  });
+
+  //switch sound
+  $('#sound-selector').change(function(){
+    // $('a[rel="nofollow self"]')
+
+    selectValue = $(this).val();
+    if (selectValue == 'kick') {
+      activeSound = kick;
+    } else if (selectValue == 'snare') {
+      activeSound = snare;
+    }
+
+    //update ui
+    $(".counter").removeClass('pad-on');
+    for(var i = 0; i < activeSound.pattern.length; i++) {
+      if (activeSound.pattern[i] == 1) {
+        var stepId = "#step" + i.toString();
+        console.log(stepId);
+        $(stepId).addClass("pad-on");
+      }
+    }
+
+  });
+
+
+
+});
