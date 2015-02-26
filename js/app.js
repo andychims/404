56f7eb9e2de95133920896bbcf51dce04583ee2f
andysplice <andy@splice.com>
2015-02-26 14:10:10 -0500

    Organize code

--- js/app.js
+++ js/app.js
@@ -1,28 +1,15 @@
 $( document ).ready(function() {
 
-
-  // var Car = function(name) {
-  //   var self = this;
-  //   self.speed = 0;
-  //   self.name = name;
-
-  //   self.beep = function(){
-  //     console.log(self.name + " says beep");
-  //   }
-  // }
-
-
-
   //
   // init
   //
 
 
   var sounds = [];
-
   var tempo = 120;
-
   var playing;
+  var intervalId;
+  var step = 0;
 
   var Sound = function(name, element_id) {
     var self = this;
@@ -44,23 +31,7 @@ $( document ).ready(function() {
   var openHat = new Sound("open-hat", "open-hat-sound");
   var closedHat = new Sound("closed-hat", "closed-hat-sound");
 
-  function playIt(){
-    playing = true;
-    setTempo(tempo);
-  };
-  
-  function stopIt(){
-    clearInterval(intervalId);
-    step = 0;
-    $(".counter-on").removeClass("counter-on");
-    playing = false;
-  };
-
-  var intervalId;
-
   var activeSound = kick;
-
-  var step = 0;
 
   var preset1 = [[1,1,0,0,1,0,0,0,1,0,0,0,0,0,0,0],
                   [1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0],
@@ -91,30 +62,15 @@ $( document ).ready(function() {
   var presets = [preset1, preset2];
   var currentPreset = presets[0];
 
-  function loadPreset(currentPreset){
-    for(var i = 0; i < sounds.length; i++) {
-      sounds[i].pattern = currentPreset[i];
-    }
-    updatePadUi();
-  };
-
   loadPreset(currentPreset);
-
-  // change preset 
-  $('#preset-selector').change(function(){
-    var selectValue = $(this).val();
-    currentPreset = presets[selectValue];
-    loadPreset(currentPreset);
-  });
-
-
-  //start playing on startup
   playIt();
 
-  //
-  //behavior every beat
-  //
 
+  //----------------------//
+  // Drum machine logic   //
+  //----------------------//
+
+  //behavior every beat
   function triggerStep(){
 
     var activeStepID = "#step"+step.toString();
@@ -137,43 +93,14 @@ $( document ).ready(function() {
     }
   };
 
-  //
-  // ui linking
-  //
-
-  // clear pattern
-  $(".clear-pattern").click(function(){
+  function setActiveSoundByName(soundName) {
     for(var i = 0; i < sounds.length; i++) {
-      sounds[i].pattern = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+      sound = sounds[i];
+      if(sound.name == soundName) {
+        activeSound = sound;
+      }
     }
-    $(".pad-on").removeClass("pad-on");
-  });
-
-  // start-stop button
-  $(".start-stop-btn").click(function(){
-    if(playing == true){
-      stopIt();
-      $(".start-stop-btn").removeClass("playing");
-    } else {
-      playIt();
-      $(".start-stop-btn").addClass("playing");
-    }
-  });
-
-
-  //sequence pad click
-  $(".pad").click(function(){
-    $(this).find(".counter").toggleClass("pad-on");
-    var pad_number = $(this).attr('rel');
-
-    if (activeSound.pattern[pad_number] == 1) {
-      activeSound.pattern[pad_number] = 0;
-    } else {
-      activeSound.pattern[pad_number] = 1;
-    }
-
-  });
-
+  }
 
   function setTempo(tempo) {
     timeout = 30000/tempo;
@@ -183,16 +110,40 @@ $( document ).ready(function() {
     };
   }
 
-  // tempo dial
-  $(".dial").knob({
-    'release' : function (v) { 
-      tempo = v;
-      setTempo(tempo);
+  function clearPreset() {
+    for(var i = 0; i < sounds.length; i++) {
+      sounds[i].pattern = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
     }
-  });
+    $(".pad-on").removeClass("pad-on");
+  }
 
+  function loadPreset(currentPreset){
+    for(var i = 0; i < sounds.length; i++) {
+      sounds[i].pattern = currentPreset[i];
+    }
+    updatePadUi();
+  };
 
-  //update pad ui
+  function playIt(){
+    playing = true;
+    setTempo(tempo);
+  };
+  
+  function stopIt(){
+    clearInterval(intervalId);
+    step = 0;
+    $(".counter-on").removeClass("counter-on");
+    playing = false;
+  };
+
+  function updatePattern(pad_number){
+  if (activeSound.pattern[pad_number] == 1) {
+      activeSound.pattern[pad_number] = 0;
+    } else {
+      activeSound.pattern[pad_number] = 1;
+    }
+  };
+
   function updatePadUi(){
     $(".counter").removeClass('pad-on');
 
@@ -204,28 +155,53 @@ $( document ).ready(function() {
     }
   }
 
-  //switch sound
+  //------------//
+  // ui linking //
+  //------------//
 
-  $("#sound-selector > button").click(function(){
-  // $('#sound-selector').change(function(){
-
-    selectValue = $(this).attr('class');
-    $(".sound-selected").removeClass("sound-selected");
-    $(this).addClass("sound-selected")
-    // selectValue = $(this).val();
-
-
-    for(var i = 0; i < sounds.length; i++) {
-      sound = sounds[i];
-      if(sound.name == selectValue) {
-        activeSound = sound;
-      }
-    };
-
-    updatePadUi();
-
+  $(".start-stop-btn").click(function(){
+    if(playing == true){
+      stopIt();
+      $(".start-stop-btn").removeClass("playing");
+    } else {
+      playIt();
+      $(".start-stop-btn").addClass("playing");
+    }
   });
 
+  $(".pad").click(function(){
+    $(this).find(".counter").toggleClass("pad-on");
+    var pad_number = $(this).attr('rel');
+    updatePattern(pad_number);
+  });
 
+  $("#sound-selector > button").click(function(){
+    soundName = $(this).attr('class');
+
+    $(".sound-selected").removeClass("sound-selected");
+    $(this).addClass("sound-selected");
+
+    setActiveSoundByName(soundName);
+    updatePadUi();
+  });
+
+  // change preset 
+  $('#preset-selector').change(function(){
+    var selectValue = $(this).val();
+    currentPreset = presets[selectValue];
+    loadPreset(currentPreset);
+  });
+
+  // clear pattern
+  $(".clear-pattern").click(function(){
+    clearPreset();
+  });
+
+  // tempo dial
+  $(".dial").knob({
+    'release' : function (tempo) { 
+      setTempo(tempo);
+    }
+  });
 
 });
